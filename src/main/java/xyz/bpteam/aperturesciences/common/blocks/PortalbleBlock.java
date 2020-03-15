d85ced37bb03e6819446d924cfeca51dc607f48d
ukminecrafted <ukminecrafted@gmail.com>
2020-03-14 19:15:47 -0500

    some basics

---
+++ src/main/java/xyz/bpteam/aperturesciences/common/blocks/PortalbleBlock.java
@@ -0,0 +1,9 @@
+package xyz.bpteam.aperturesciences.common.blocks;
+
+import net.minecraft.block.Block;
+
+public class PortalbleBlock extends Block {
+    public PortalbleBlock(Properties properties) {
+        super(properties);
+    }
+}
---
+++ src/main/java/xyz/bpteam/aperturesciences/common/init/ModBlocks.java
@@ -0,0 +1,10 @@
+package xyz.bpteam.aperturesciences.common.init;
+
+import net.minecraft.block.Block;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ModBlocks {
+    public static final List <Block> BLOCKS = new ArrayList<Block>();
+}

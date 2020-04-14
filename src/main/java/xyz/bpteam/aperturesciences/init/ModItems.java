3ab2fed4740e9f711aeb529924be745311b7bb8e
UKMinecrafted <ukminecrafted@gmail.com>
2020-04-14 01:39:39 -0500

    some more set up with portal guns

--- src/main/java/xyz/bpteam/aperturesciences/init/ModItems.java
+++ src/main/java/xyz/bpteam/aperturesciences/init/ModItems.java
@@ -6,7 +6,6 @@ import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import xyz.bpteam.aperturesciences.ApertureSciences;
-import xyz.bpteam.aperturesciences.common.items.ItemBase;
 import xyz.bpteam.aperturesciences.common.items.ItemPortalGun;
 
 public class ModItems {
